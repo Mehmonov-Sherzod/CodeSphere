fa3a4200c4d36ee088ea4606634c96917fef4f5a
Abdulloh2045 <gafurjonovabdulloh23@gmail.com>
2026-01-14 14:15:19 +0500

    sdfsdf

---
+++ src/CodeSphere.Application/Models/Jwt/JwtSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSphere.Application.Models.Jwt
+{
+    public class JwtSettings
+    {
+        public string Issuer { get; set; } = null;
+        public string Audience { get; set; } = null;
+        public string Key { get; set; } = null;
+        public int? TokenValidityMins { get; set; } = null;
+    }
+}
